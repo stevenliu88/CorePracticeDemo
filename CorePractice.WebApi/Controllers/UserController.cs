c70647dc8a8273a004caf64e8da10e18edb197ca
lei liu <liuleiwangyue@gmail.com>
2020-01-17 16:26:13 +1100

    Create User Controller that used to handle request. However it occurred loading error. Need to debug and fix it.

---
+++ CorePractice.WebApi/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CorePractice.Application.Models;
+using CorePractice.Application.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CorePractice.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<User> GetUser(int userId)
+        {
+            return await _userService.GetUserByIdAsync(userId);
+        }
+        [HttpPost]
+        public async Task AddUser(User user)
+        {
+            await _userService.InsertUserAsync(user);
+        }
+
+        [HttpDelete]
+        public async Task DeleteUser(int id)
+        {
+            await _userService.DeleteUserByIdAsync(id);
+        }
+    }
+}
