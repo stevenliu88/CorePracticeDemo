a6b87c45ceceab5b3143b949eb8dc4431158a788
lei liu <liuleiwangyue@gmail.com>
2020-01-17 14:33:25 +1100

    Create Models base on requirements

---
+++ CorePractice.Application/Models/Group.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CorePractice.Application.Models
+{
+    public class Group
+    {
+        public int GroupId { get; set; }
+
+        public string GroupName { get; set; }
+
+        public string Description { get; set; }
+    }
+}
