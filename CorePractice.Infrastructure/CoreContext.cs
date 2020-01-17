71631f8921cc14dcc4eadf5c9cf9dfee07f5003e
lei liu <liuleiwangyue@gmail.com>
2020-01-17 14:46:42 +1100

    Create CoreContext and try to add-migration(Update database).

---
+++ CorePractice.Infrastructure/CoreContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CorePractice.Application.Models;
+
+namespace CorePractice.Infrastructure
+{
+    public class CoreContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        private const string connectionString = @"server=coresample.database.windows.net; database=coresample;user id=coredev;password='s+_v_#anXLZHzzpzAgT'";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<User>(entity =>
+            {
+                entity.HasKey(user => user.UserId);
+            });
+        }
+    }
+}
