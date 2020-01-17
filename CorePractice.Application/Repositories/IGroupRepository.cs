7900ad4024050533514b636a41f66c6003b2f347
lei liu <liuleiwangyue@gmail.com>
2020-01-17 15:10:06 +1100

    Create Repositories and webApi project(will handle restful API request)

---
+++ CorePractice.Application/Repositories/IGroupRepository.cs
@@ -0,0 +1,16 @@
+﻿using CorePractice.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorePractice.Application.Repositories
+{
+    interface IGroupRepository
+    {
+        Task<Group> GetGroupByIdAsync(int groupId);
+        Task InsertGroupAsync(Group group);
+        Task UpdateGroupAsync(Group group);
+        Task DeleteGroupByIdAsync(int groupId);
+    }
+}
---
+++ CorePractice.Application/Repositories/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using CorePractice.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorePractice.Application.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserByIdAsync(int id);
+
+        Task InsertUserAsync(User user);
+        Task UpdateUserAsync(User user);
+        Task DeleteUserByIdAsync(int id);
+
+    }
+}
